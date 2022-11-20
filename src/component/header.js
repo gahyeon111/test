1be2bef08422e0c3fdd437659b02477bfdbc67af
gahyeon111 <105411573+gahyeon111@users.noreply.github.com>
2022-11-20 17:15:57 +0900

    header

--- src/component/header.js
+++ src/component/header.js
@@ -3,7 +3,7 @@ function Header() {
         <div className='App-header'>
             <div className='h-body'>
                 <div className='h-headline'>PDF Summarizer</div>
-                <div className='h-moreinfo'>More Info.</div>
+                {/* <div className='h-moreinfo'>More Info.</div> */}
                 <a className="h-aboutus" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >ABOUT US</a>
             </div>
         </div>
